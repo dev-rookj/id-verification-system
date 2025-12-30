3d321d8bee1fcedc132317f2776989af37ed8352
dev-rookj <jeffersonmichaelriosa07@gmail.com>
2025-12-31 01:53:01 +0800

    SQLite database initialized

---
+++ id-verification-system/db-config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace id_verification_system
+{
+    public class db_config
+    {
+        // dito maglalagay ng functions, or pwede rin naman sa forms na lang mismo
+    }
+}
